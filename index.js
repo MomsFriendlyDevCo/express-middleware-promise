e4a2d61ae8adca040a6f47b93b5a24dd94ab59a6
Matt Carter <m@ttcarter.com>
2018-06-01 13:37:32 +1000

    Test cases all working

--- index.js
+++ index.js
@@ -11,8 +11,21 @@ module.exports = function(options) {
 	var settings = {
 		app: undefined,
 		promise: Promise,
-		isPromise: obj => !! obj.then,
+		isPromise: obj => obj && obj.then,
+		isResolvable: obj =>
+			obj
+			&& !(obj instanceof http.ServerResponse)
+			&& (
+				_.isPlainObject(obj)
+				|| _.isString(obj)
+				|| _.isNumber(obj)
+				|| _.isDate(obj)
+				|| _.isArrayLike(obj)
+				|| _.isBuffer(obj)
+				|| _.isBoolean(obj)
+			),
 		resolve: (req, res, data) => res.send(_.isNumber(data) ? '' + data : data),
+		resolveEmpty: (req, res, data) => res.sendStatus(200),
 		error: (req, res, data) => res.sendStatus(400).send(data),
 		wrapFunctions: ['get'],
 		...options,
@@ -23,23 +36,34 @@ module.exports = function(options) {
 		settings.app[func] = function(...args) {
 			// Delegate to normal app
 			return expressHandler.apply(settings.app, args.map(arg => {
-				if (!_.isFunction(arg)) return arg; // Probably a string path - ignore
-
-				return function(req, res, next) {
-					var result = arg.call(req, req, res, next);
-					if (settings.isPromise(result)) {
-						console.log('RES TYPE', result);
-						result
-							.then(promiseRes => {
-								settings.resolve(req, res, promiseRes);
-							})
-							.catch(promiseErr => {
-								settings.error(req, res, promiseErr);
-							})
-					} else if (result && !(result instanceof http.ServerResponse)) { // We got a truthy value - assume its a scalar response
-						settings.resolve(req, res, result);
-					}
-				};
+				if (_.isString(arg) || _.isRegExp(arg)) { // Prefix string or matcher - pass on unedited
+					return arg;
+				} else if (_.isFunction(arg)) { // Middleware callback function - wrap in handler
+					return function(req, res, next) {
+						var result = arg.call(req, req, res, next);
+						if (settings.isPromise(result)) {
+							result
+								.then(promiseRes => {
+									if (promiseRes) { // Returned a value
+										settings.resolve(req, res, promiseRes);
+									} else if (_.isFunction(next)) { // No value and this promise is in the middle of a chain
+										next();
+									} else { // No value and this promise is the end of a chain
+										settings.resolveEmpty(req, res, promiseRes);
+									}
+								})
+								.catch(promiseErr => {
+									settings.error(req, res, promiseErr);
+								})
+						} else if (settings.isResolvable(result)) { // We got a truthy value - assume its a scalar response
+							settings.resolve(req, res, result);
+						}
+					};
+				} else if (settings.isPromise(arg)) { // Promise return
+					throw new Error('Promises cannot be passed when constructing a route as they only resolve once - pass in a promise factory function instead');
+				} else { // Unknown object
+					throw new Error('Unsupported object while constructing a route');
+				}
 			}));
 		};
 	});
--- test/index.js
+++ test/index.js
@@ -12,20 +12,69 @@ var server;
 var port = 8181;
 var url = 'http://localhost:' + port;
 
-before('setup server', done => {
+before('Setup server', done => {
 	app.use(expressLogger);
 	app.use(bodyParser.json());
 	app.set('log.indent', '      ');
 
 	expressAsPromised(app);
 
+	// Callbacks {{{
 	app.get('/callbacks/string', (req, res) => res.send('Hello'));
 	app.get('/callbacks/number', (req, res) => res.json(123));
 	app.get('/callbacks/object', (req, res) => res.send({foo: [1, 2, 3], bar: 'Bar!'}));
+	app.get('/callbacks/defer', (req, res) => setTimeout(()=> res.send('Defer!'), 100));
+	// }}}
 
+	// Promises {{{
 	app.get('/promises/string', (req, res) => 'Hello');
 	app.get('/promises/number', (req, res) => 123);
 	app.get('/promises/object', (req, res) => ({foo: [1, 2, 3], bar: 'Bar!'}));
+	app.get('/promises/defer', (req, res) => new Promise(resolve => setTimeout(()=> resolve('Defer!'), 100)));
+
+	app.get('/promises/chain', (req, res) =>
+		Promise.resolve()
+			.then(()=> 'Foo')
+			.then(()=> 'Bar')
+			.then(()=> 'Baz')
+	);
+
+	app.get('/promises/all', (req, res) =>
+		Promise.all([
+			new Promise(resolve => setTimeout(()=> resolve('Foo'), 100)),
+			new Promise(resolve => setTimeout(()=> resolve('Bar'), 50)),
+			new Promise(resolve => setTimeout(()=> resolve('Baz'), 10)),
+		])
+	);
+	// }}}
+
+	// Use cases {{{
+	app.get('/use-cases/1',
+		(req, res, next) => new Promise(resolve => setTimeout(()=> resolve(), 10)),
+		(req, res, next) => new Promise(resolve => setTimeout(()=> resolve(), 20)),
+		(req, res, next) => new Promise(resolve => setTimeout(()=> resolve(), 30)),
+		(req, res) => res.send('Done!'),
+	);
+
+	app.get('/use-cases/2',
+		(req, res, next) => next(),
+		(req, res, next) => setTimeout(()=> next(), 10),
+		(req, res, next) => new Promise(resolve => setTimeout(()=> resolve(), 20)),
+		(req, res) => res.send('Done!'),
+	);
+
+	app.get('/use-cases/3',
+		(req, res, next) => new Promise(resolve => setTimeout(()=> resolve(), 10)),
+		(req, res, next) => setTimeout(()=> next(), 20),
+		(req, res, next) => new Promise(resolve => setTimeout(()=> resolve('Done!'), 30)),
+	);
+
+	app.get('/use-cases/4',
+		(req, res, next) => new Promise(resolve => setTimeout(()=> resolve(), 10)),
+		(req, res, next) => new Promise((resolve, reject) => reject('Nope!')),
+		(req, res, next) => new Promise(resolve => setTimeout(()=> resolve('Done!'), 30)),
+	);
+	// }}}
 
 	server = app.listen(port, null, function(err) {
 		if (err) return done(err);
@@ -34,7 +83,7 @@ before('setup server', done => {
 	});
 });
 
-after('close server', done => server.close(done));
+after('Teardown server', done => server.close(done));
 
 describe('Normal callbacks', ()=> {
 
@@ -54,6 +103,12 @@ describe('Normal callbacks', ()=> {
 		superagent
 			.get(`${url}/callbacks/object`)
 			.then(res => expect(res.body).to.be.nested.deep.equal({foo: [1, 2, 3], bar: 'Bar!'}))
+	);
+
+	it('should handle defered responses', ()=>
+		superagent
+			.get(`${url}/callbacks/defer`)
+			.then(res => expect(res.text).to.be.equal('Defer!'))
 	);
 
 });
@@ -77,5 +132,65 @@ describe('Promise support', ()=> {
 			.get(`${url}/promises/object`)
 			.then(res => expect(res.body).to.be.nested.deep.equal({foo: [1, 2, 3], bar: 'Bar!'}))
 	);
+
+	it('should handle defered responses', ()=>
+		superagent
+			.get(`${url}/promises/defer`)
+			.then(res => expect(res.text).to.be.equal('Defer!'))
+	);
+
+	it('should correctly resolve promise chains', ()=>
+		superagent
+			.get(`${url}/promises/chain`)
+			.then(res => expect(res.text).to.be.equal('Baz'))
+	);
+
+	it('should correctly resolve promise arrays', ()=>
+		superagent
+			.get(`${url}/promises/all`)
+			.then(res => expect(res.body).to.be.nested.deep.equal(['Foo', 'Bar', 'Baz']))
+	);
+
+});
+
+describe('Use cases', ()=> {
+
+	it('should correctly resolve [Promise, Promise, Promise, res.send()]', ()=>
+		superagent
+			.get(`${url}/use-cases/1`)
+			.then(res => expect(res.text).to.be.equal('Done!'))
+	);
+
+	it('should correctly resolve [cb, cb + delay, Promise, res.send()]', ()=>
+		superagent
+			.get(`${url}/use-cases/2`)
+			.then(res => expect(res.text).to.be.equal('Done!'))
+	);
+
+	it('should correctly resolve [Promise, cb, Promise + resolve value]', ()=>
+		superagent
+			.get(`${url}/use-cases/3`)
+			.then(res => expect(res.text).to.be.equal('Done!'))
+	);
+
+	it('should correctly reject [Promise, Promise + reject, Promise]', ()=>
+		superagent
+			.get(`${url}/use-cases/3`)
+			.catch(err => expect(err.response.text).to.be.equal('Nope!'))
+	);
+
+});
+
+describe('Error catching', ()=> {
+
+	it('should throw if using promises in route construction', ()=>
+		expect(()=> {
+			app.get('/error/promiseMiddleware',
+				new Promise(resolve => setTimeout(()=> resolve('Foo'), 100)),
+				new Promise(resolve => setTimeout(()=> resolve('Bar'), 50)),
+				new Promise(resolve => setTimeout(()=> resolve('Baz'), 10)),
+			);
+		}).to.throw
+	)
 
 });
